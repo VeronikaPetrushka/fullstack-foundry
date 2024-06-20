fd460c2f0f3319e27f326ffb7b5722b6e9e38f6e
Zenoviy Fil <fil.zenoviy@gmail.com>
2024-06-20 14:20:17 +0200

    user auth

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -4,7 +4,15 @@
 // створити приватний ендпоінт для логаута користувача
 
 import express from 'express';
+import { logIn, logOut, register } from 'backend/authControllers/authControllers.js';
+import { loginSchema, registerSchema } from 'backend/models/user.js';
+import validateBody from 'backend/middlewares/validateBody.js';
+import auth from 'backend/middlewares/authenticate.js';
 
 const authRouter = express.Router();
+
+authRouter.post('/register', validateBody(registerSchema), register);
+authRouter.post('/login', validateBody(loginSchema), logIn);
+authRouter.post('/logout', auth, logOut);
 
 export default authRouter;
