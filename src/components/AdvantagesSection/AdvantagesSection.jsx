f65a53d5e758745e74b750cd764a1b2defe4afe5
Sashiy1 <aleksander.paluch@wp.pl>
2024-06-22 11:34:51 +0200

    welcome section done

---
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -0,0 +1,59 @@
+
+import css from './AdvantagesSection.module.css';
+
+
+
+export const AdvantagesSection = () => {
+  
+
+  return (
+  <div className={css.mojdiv}>
+    <h1>hello</h1>
+  </div>
+
+  );
+};
+
+
+
+
+
+
+
+
+
+// <div className={css.section}>
+      
+// <div className={css.customersBox}>
+//   <ul className={css.customerImg}>
+//     <li className={css.item}>
+//       <img className={css.img} src={girl2} alt="girl1" />
+//     </li>
+
+//     <li className={css.item}>
+//       <img className={css.img} src={boy} alt="boy" />
+//     </li>
+
+//     <li className={css.item}>
+//       <img className={css.img} src={girl1} alt="girl2" />
+//     </li>
+//   </ul>
+
+
+// </div>
+
+// <div className={css.group}>
+//   <ul className={css.advantagesGroup}>
+//     <li className={css.textAdvantage}>
+//       <div className={css.ellipse}></div>
+//       <p className={css.text}>Habit drive</p>
+//     </li>
+//     <li className={css.textAdvantage}>
+//       <p className={css.textBlack}>View statistics</p>
+//     </li>
+//     <li className={css.textAdvantage}>
+//       <p className={css.textBlack}>Personal rate setting</p>
+//     </li>
+//   </ul>
+// </div>
+// </div>
