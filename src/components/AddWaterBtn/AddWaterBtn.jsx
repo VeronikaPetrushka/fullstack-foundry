79bf14cfd01939914509c21c6c45398c5a4bdee5
anyalenzy <anya.dudnik.91@gmail.com>
2024-06-24 01:45:10 +0300

    add WaterBtn , change path to img

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,2 +1,29 @@
-const AddWaterBtn = () => {};
+import css from './AddWaterBtn.module.css';
+import Icon from '../Icon/Icon';
+
+const AddWaterBtn = ({ isBig = false }) => {
+  return (
+    <div className={css.addBtnWrap}>
+      <button
+        className={isBig ? css.btnBig : css.btnSmall}
+        type="button"
+        // onClick={openWaterModal}
+      >
+        {isBig ? (
+          <Icon iconName="plus" width="18" height="18" styles={css.iconWhite} />
+        ) : (
+          <div className={css.iconWrapper}>
+            <Icon
+              iconName="plus"
+              width="18"
+              height="18"
+              styles={css.iconGrey}
+            />
+          </div>
+        )}
+        <span className={isBig ? css.txtSmall : css.txtBig}>Add water</span>
+      </button>
+    </div>
+  );
+};
 export default AddWaterBtn;
