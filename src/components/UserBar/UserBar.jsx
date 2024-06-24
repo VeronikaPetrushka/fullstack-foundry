d9e1c5f97fc560529da99178fc14755810ab1842
Veronika <veronikapetrushka@gmail.com>
2024-06-24 14:47:51 +0200

    UserBar, UserBarPopover, UserPanel fully done / adaptation left

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,30 +1,38 @@
 import css from './UserBar.module.css'
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-// import UserBarPopover from "../UserBarPopover/UserBarPopover";
+import UserBarPopover from "../UserBarPopover/UserBarPopover";
 import { selectUserName, selectAvatar } from '../../redux/user/selectors';
+import Icon from '../Icon/Icon';
 
 const UserBar = () => {
+    const [isPopoverActive, setIsPopoverActive] = useState(false);
+    const userName = useSelector(selectUserName);
+    const avatar = useSelector(selectAvatar);
 
-    const userName = useSelector(selectUserName)
-    const avatar = useSelector(selectAvatar)
+    const handleButtonClick = () => {
+        setIsPopoverActive(prevState => !prevState);
+    };
 
     return (
         <div className={css.userBarWrapper}>
-            {userName ? (
-                    <p className={css.userName}>{userName}</p>
-                ) : (
-                    <p className={css.userName}>You</p>
-                )}
-            {avatar ?
+            <div className={css.userBarMainWrapper}>
+                <p className={css.userName}>{userName || 'You'}</p>
                 <div className={css.avatarBox}>
-                    <img src={avatar} className={css.avatarImg} />
-                </div> : 
-                <div className={css.avatarBox}>
-                    <img src="src\assets\img\avatar-default.jpg" className={css.avatarImg}/>
-                </div>}
-        {/* <UserBarPopover /> */}
+                    <img src={avatar || 'src/assets/img/avatar-default.jpg'} className={avatar ? css.avatarImg : css.avatarImgdefault} />
+                </div>
+                <button
+                    type="button"
+                    className={`${css.popoverBtn} ${isPopoverActive ? css.isActive : ''}`}
+                    onClick={handleButtonClick}
+                >
+                    <Icon iconName="arr-up" width='18' height='18' styles={css.arrUpIcon} />
+                </button>
+            </div>
+            {isPopoverActive && <UserBarPopover />}
         </div>
-    )
-}
+    );
+};
+
 
 export default UserBar;
