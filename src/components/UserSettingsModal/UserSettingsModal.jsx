4e7cc66ae9b197df37c1b64320204a55677c1b4d
Nataliia-Nazarko <you@example.com>
2024-06-24 11:05:40 +0200

    update UserSettingsModal

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,13 +1,29 @@
-import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import css from './UserSettingsModal.module.css';
+//import { selectUser } from '../../../redux/auth/selectors';
+import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
 
-const UserSettingsModal = () => {
+export default function UserSettingsModal({ closeModal, modIsOpen }) {
+  const [get_setting, setGet_setting] = useState([]);
+
+  const selector = useSelector(selectUser);
+
+  useEffect(() => {
+    if (!modIsOpen) {
+      return;
+    }
+    setGet_setting(selector);
+  }, [modIsOpen]);
+
   return (
-    <div className={css.settings}>
-      <h1>Setting</h1>
-      <UserSettingsForm />;
-    </div>
+    <>
+      <h2 className={css.title}> Setting</h2>
+      <div className={css.settingBox}>
+        {get_setting.length !== 0 && (
+          <UserSettingsForm closeModal={closeModal} getSetting={get_setting} />
+        )}
+      </div>
+    </>
   );
-};
-
-export default UserSettingsModal;
+}
