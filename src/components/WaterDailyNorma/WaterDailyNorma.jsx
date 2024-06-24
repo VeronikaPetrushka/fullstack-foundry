c206ef6a13fdb973accf01bfe526d3931b9c6668
anyalenzy <anya.dudnik.91@gmail.com>
2024-06-24 10:18:42 +0300

    done style for mob

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,2 +1,17 @@
-const WaterDailyNorma = () => {};
+// import { useSelector } from 'react-redux';
+// import { selectDailyNorma } from '../../redux/user/selectors';
+import css from './WaterDailyNorma.module.css';
+
+const WaterDailyNorma = () => {
+  //   const dailyNorma = useSelector(selectDailyNorma);
+  //   const dailyNormaL = dailyNorma / 1000 || 2;
+  const dailyNormaL = 2;
+
+  return (
+    <div className={css.dailyNormaWrap}>
+      <p className={css.quantity}>{dailyNormaL} L</p>
+      <p className={css.desc}>My daily norma</p>
+    </div>
+  );
+};
 export default WaterDailyNorma;
