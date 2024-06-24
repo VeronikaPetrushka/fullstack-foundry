fe36d6186d1800e934f85029f27dc591c145e027
Zenoviy Fil <fil.zenoviy@gmail.com>
2024-06-24 11:00:54 +0200

    water form

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import { WelcomeSection } from '../../components/WelcomeSection/WelcomeSection';
 import { AdvantagesSection } from '../../components/AdvantagesSection/AdvantagesSection';
 import Container from '../../components/Container/Container';
 import Page from '../../components/Page/Page';
+// import WaterForm from '../../components/WaterForm/WaterForm';
 
 const Homepage = () => {
   return (
@@ -9,6 +10,7 @@ const Homepage = () => {
       <Page>
         <WelcomeSection />
         <AdvantagesSection />
+        {/* <WaterForm isOpen={true}/> */}
       </Page>
     </Container>
   );
