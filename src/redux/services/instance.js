a6dcfd9fd98c0b4f69601afccb00401d43a8175d
Veronika <veronikapetrushka@gmail.com>
2024-06-25 14:33:48 +0200

    signin / signup -> redirect to trackerPage, logout -> redirect to homePage

--- src/redux/services/instance.js
+++ src/redux/services/instance.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-сonst API_URI = 'https://aquatrack-api-myzh.onrender.com/api';
-  //const API_URI = 'http://localhost:8080/api';
-
+const API_URI = 'https://aquatrack-api-myzh.onrender.com/api';
+//const API_URI = 'http://localhost:8080/api';
 
 const instance = axios.create({
   baseURL: API_URI,
@@ -10,7 +9,7 @@ const instance = axios.create({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-  }
+  },
 });
 
 instance.interceptors.request.use(
@@ -36,13 +35,12 @@ instance.interceptors.response.use(
       originalRequest._retry = true;
 
       try {
-        
         const { data } = await axios.get(`${API_URI}/auth/refresh`, {
           withCredentials: true,
           headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-          }
+          },
         });
 
         localStorage.setItem('token', data.token);
