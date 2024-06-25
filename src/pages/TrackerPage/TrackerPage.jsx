a590564e21e746e44841f972097a602247dc725d
demchukv <demchukv@gmail.com>
2024-06-25 14:42:32 +0300

    calendar

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -2,18 +2,21 @@ import Page from '../../components/Page/Page';
 import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
 import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { userInfo } from '../../redux/user/operations';
 import Calendar from '../../components/Calendar/Calendar';
 import css from './TrackerPage.module.css';
 
 const TrackerPage = () => {
+  const [selectedDate, setSelectedDate] = useState();
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(userInfo());
   });
 
-  const handleCalendarBtnClick = (selectedDate) => {
+  const handleCalendarBtnClick = (btnDate) => {
+    setSelectedDate(btnDate);
     console.log(selectedDate);
   };
 
