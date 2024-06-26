6afce064384a3431fc1f5e8f7ba9b4afeb71a726
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-06-26 13:55:25 +0300

    frontend

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import css from './SignInForm.module.css';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -7,7 +7,7 @@ import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../../redux/auth/operations';
 import { selectIsSignedIn } from '../../redux/auth/selectors';
-
+import icon from '../../assets/icons.svg';
 
 const schema = yup.object().shape({
   email: yup.string().email('Invalid email').required('Email is required'),
@@ -21,6 +21,8 @@ const SignInForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const isSignedIn = useSelector(selectIsSignedIn);
+  const [inputTypePassword, setTypePassword] = useState('password');
+  const [iconPassword, setIconPassword] = useState('eye');
 
   const {
     register,
@@ -42,6 +44,13 @@ const SignInForm = () => {
     }
   }, [isSignedIn, navigate]);
 
+  const toggleShowPassword = () => {
+    setTypePassword(prevType =>
+      prevType === 'password' ? 'text' : 'password'
+    );
+    setIconPassword(prevIcon => (prevIcon === 'eye' ? 'eye-off' : 'eye'));
+  };
+
   return (
     <div className={css.signUpWrap}>
       <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
@@ -57,12 +66,29 @@ const SignInForm = () => {
         )}
 
         <label className={css.label}>Password</label>
-        <input
-          className={`${css.input} ${errors.password ? css.inputError : ''}`}
-          {...register('password')}
-          type="password"
-          placeholder="Enter your password"
-        />
+        <div className={css.inputWrapper}>
+          <input
+            className={`${css.input} ${errors.password ? css.inputError : ''}`}
+            {...register('password')}
+            type={inputTypePassword}
+            placeholder="Enter your password"
+          />
+          <button
+            type="button"
+            onClick={toggleShowPassword}
+            className={css.iconButton}
+          >
+            {iconPassword === 'eye' ? (
+              <svg className={css.icon}>
+                <use href={`${icon}#eye`} />
+              </svg>
+            ) : (
+              <svg className={css.icon}>
+                <use href={`${icon}#eye-off`} />
+              </svg>
+            )}
+          </button>
+        </div>
         {errors.password && (
           <p className={css.errorMessage}>{errors.password.message}</p>
         )}
