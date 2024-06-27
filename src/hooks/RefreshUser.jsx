50ef8a368d9da08714ba1742f6072a6f0eda138d
demchukv <demchukv@gmail.com>
2024-06-27 12:42:15 +0300

    add Google Sign In

--- src/hooks/RefreshUser.jsx
+++ src/hooks/RefreshUser.jsx
@@ -1,12 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsSignedIn } from '../redux/auth/selectors';
 import { tokenRefresh } from '../redux/auth/operations';
+import { useSearchParams } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 
 export const useRefreshUser = () => {
 
+  const [searchParams] = useSearchParams();
+  const gToken = searchParams.get('token');
+
+  if(gToken) {
+    const decoded = jwtDecode(gToken);
+    const current = new Date();
+    if (decoded.exp * 1000 > current.getTime()) {
+      localStorage.setItem('token', gToken);
+    }
+  }
+
   const dispatch = useDispatch();
   let isSignedIn = useSelector(selectIsSignedIn);
+
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -15,6 +29,7 @@ export const useRefreshUser = () => {
     }
   }, [dispatch]);
   isSignedIn = useSelector(selectIsSignedIn);
+
 
   return [isSignedIn];
 
