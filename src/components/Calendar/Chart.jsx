efb28eb8612e78a677ccd4803ac24ce6e953dee3
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-06-27 19:17:48 +0300

    add logic for Chart component

--- src/components/Calendar/Chart.jsx
+++ src/components/Calendar/Chart.jsx
@@ -1,5 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+
+import { weekActivity } from '../../redux/water/operations';
+import getSevenDayRange from '../../helpers/getSevenDaysRange';
+
 const Chart = ({ selectedDate }) => {
-  console.log('Selected date:', selectedDate);
+  const dispatch = useDispatch();
+  const [weekDate, setWeekDate] = useState(null);
+
+  useEffect(() => {
+    setWeekDate(getSevenDayRange(selectedDate));
+  }, [selectedDate]);
+
+  useEffect(() => {
+    if (weekDate) {
+      dispatch(weekActivity(weekDate));
+    }
+  }, [dispatch, weekDate]);
+
   return <div>Chart</div>;
 };
 
