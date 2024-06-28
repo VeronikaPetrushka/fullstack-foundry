7bb23496b7b80dcaa887cc87decfd6689066fd31
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-06-28 12:14:39 +0300

    Chart done

--- src/components/Calendar/Chart.jsx
+++ src/components/Calendar/Chart.jsx
@@ -1,10 +1,22 @@
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useMemo, useState } from 'react';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
 import { weekActivity } from '../../redux/water/operations';
+import { selectWaterWeekly } from '../../redux/water/selectors';
 import getSevenDayRange from '../../helpers/getSevenDaysRange';
+import fillMissingDays from '../../helpers/fillMissingDays';
 
 const Chart = ({ selectedDate }) => {
+  const waterWeekly = useSelector(selectWaterWeekly);
   const dispatch = useDispatch();
   const [weekDate, setWeekDate] = useState(null);
 
@@ -18,7 +30,58 @@ const Chart = ({ selectedDate }) => {
     }
   }, [dispatch, weekDate]);
 
-  return <div>Chart</div>;
+  const data = useMemo(() => {
+    if (waterWeekly && weekDate) {
+      return fillMissingDays(weekDate, waterWeekly);
+    }
+  }, [waterWeekly, weekDate]);
+
+  return (
+    data && (
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart data={data}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#9BE1A0" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#9BE1A0" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="day" />
+          <YAxis />
+          <Tooltip
+            contentStyle={{
+              backgroundColor: '#FFFFFF',
+              color: '#323F47',
+              fontWeight: 'bold',
+            }}
+            labelStyle={{ color: '#323F47' }}
+            itemStyle={{ color: '#323F47' }}
+            cursor={false}
+            offset={15}
+            formatter={value => `${value} ml`}
+            labelFormatter={() => ``}
+          />
+          <Area
+            type="monotone"
+            dataKey="totalAmount"
+            stroke="#87D28D"
+            fill="url(#colorUv)"
+            fillOpacity={1}
+            activeDot={{
+              r: 8,
+              fill: '#87D28D',
+              stroke: '#ffffff',
+              strokeWidth: 2,
+            }}
+            dot={{ r: 8, fill: '#ffffff', stroke: '#87D28D', strokeWidth: 2 }}
+            strokeWidth={2.5}
+          ></Area>
+        </AreaChart>
+      </ResponsiveContainer>
+    )
+  );
 };
 
 export default Chart;
