a9c0bc3eb4aee5580d8d6984469f7de986a1e370
demchukv <demchukv@gmail.com>
2024-06-28 13:15:15 +0300

    add/edit/delete water slice and operation

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -3,8 +3,8 @@ import CalendarItem from '../CalendarItem/CalendarItem';
 import css from './Calendar.module.css';
 
 
-import { useDispatch } from 'react-redux';
-import {addWater, deleteWater} from '../../redux/water/operations';
+// import { useDispatch } from 'react-redux';
+// import {addWater, deleteWater} from '../../redux/water/operations';
 
 const Calendar = ({
   daysOfSelectedMonth,
@@ -14,21 +14,30 @@ const Calendar = ({
   handleClick,
 }) => {
 
-  const dispatch = useDispatch();
-  const handleAddWater = async () => {
-    const data = {
-      "amount": 230,
-      "date": selectedDate.fullDate+"T10:30"
-    }
-    await dispatch(addWater(data));
-}
+//   const dispatch = useDispatch();
+//   const handleAddWater = async () => {
+//     const data = {
+//       "amount": 230,
+//       "date": selectedDate.fullDate+"T10:30"
+//     }
+//     await dispatch(addWater(data));
+// }
 
-  const handleDeleteWater = async () => {
-    const data = {
-      "id": "667e7354dd629e109a93646e"
-    }
-    await dispatch(deleteWater(data));
-}
+// const handleEditWater = async () => {
+//   const id = "667e738ddd629e109a93647f";
+//   const data = {
+//     "amount": 370,
+//     "date": selectedDate.fullDate+"T13:30"
+//   }
+//   await dispatch(addWater(data));
+// }
+
+//   const handleDeleteWater = async () => {
+//     const data = {
+//       "id": "667e7354dd629e109a93646e"
+//     }
+//     await dispatch(deleteWater(data));
+// }
 
 
   return (
@@ -46,10 +55,11 @@ const Calendar = ({
         </div>
       ))}
 
-      <div style={{position: 'absolute', right: '0px', bottom: '-20px'}}>
+      {/* <div style={{position: 'absolute', right: '0px', bottom: '-20px'}}>
       <button onClick={handleAddWater}>Add water</button>
+      <button onClick={handleEditWater}>Edit water</button>
       <button onClick={handleDeleteWater}>Delete water</button>
-      </div>
+      </div> */}
 
     </div>
   );
